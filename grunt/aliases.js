a9389fc05afe39a11668fb2ba27bfff6a28c7334
Ismailov Shuhrat <admin@ismax.ru>
2015-04-17 18:05:41 +0400

    Сборка статики

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -1,6 +1,8 @@
 module.exports = {
     'default': [
         'jslint',
+        'requirejs',
+        'cssmin',
         'mochaTest'
     ]
 };
--- grunt/cssmin.js
+++ grunt/cssmin.js
@@ -6,5 +6,14 @@ module.exports = {
         files: {
             'hosts/base/static/build/index.css': ['hosts/base/static/css/common.css', 'hosts/base/static/css/index.css']
         }
+    },
+
+    index: {
+        options: {
+            banner: '/* Index minified */'
+        },
+        files: {
+            'hosts/base/static/build/models.css': ['hosts/base/static/css/common.css', 'hosts/base/static/css/models.css']
+        }
     }
 };

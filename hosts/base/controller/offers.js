6ccb03af669870b87d1921a07d076bdead6c242e
Ismailov Shukhrat <s.m.ismailov@tinkoff.ru>
2015-06-09 12:43:34 +0300

    Fix

--- hosts/base/controller/offers.js
+++ hosts/base/controller/offers.js
@@ -64,58 +64,26 @@ exports.csv = function (req, res, next) {
 
     delete req.params.modelId;
     delete req.params.page;
-    if (!req.params.hasOwnProperty('geo_id')) {
-        req.params.remote_ip = req.headers['x-forwarded-for'];
-    }
 
-    function _request(page, accept) {
-        var request = http.request({
-                host:     '194.58.98.18',
-                port:     3000,
-                path:     '/v1/model/' + modelId + '/offers.json?page=' + page + '&' + querystring.stringify(req.params),
-                method:   'GET',
-                headers: {
-                    'Host':                 'market.icsystem.ru',
-                    'X-Ismax-Key':          '85d1fb3b78dfab1d14aebdb44d78eb9ff6b9811515e0698078ad93d7477dc370',
-                    'X-Forwarded-Proto':    'http',
-                    'X-Forwarded-for':      req.headers['x-forwarded-for']
+    function loop(page, accept){
+        req.api('/v1/model/' + modelId + '/offers.json?page=' + page + '&' + querystring.stringify(req.params), function (status, data) {
+            var _data = JSON.parse(data);
+            if (status === 200) {
+                if (_data.hasOwnProperty('offers') && _data.offers.count > 0) {
+                    result = result.concat(_data.offers.items);
+                    _request(_data.offers.page + 1, accept);
+                } else {
+                    accept(200);
                 }
-            }, function (response) {
-                var data = '';
-
-                response.on('data', function (chunk) {
-                    data += chunk.toString();
-                });
-
-                response.on('end', function () {
-                    var _data = JSON.parse(data);
-                    if (response.statusCode === 200) {
-                        if (_data.hasOwnProperty('offers') && _data.offers.count > 0) {
-                            result = result.concat(_data.offers.items);
-                            _request(_data.offers.page + 1, accept);
-                        } else {
-                            accept(200);
-                        }
-                    } else {
-                        result = _data;
-                        accept(response.statusCode);
-                    }
-                });
-            });
-
-        request.on('error', function (err) {
-            result = {
-                errors: [err.message]
-            };
-
-            accept(500);
+            } else {
+                result = _data;
+                accept(status);
+            }
         });
-
-        request.end();
     }
 
-    _request(1, function (statusCode) {
-        res.statusCode = statusCode;
+    loop(1, function (status) {
+        res.statusCode = status;
         res.setHeader('Content-Type', 'application-json; charset=UTF-8');
         res.end(JSON.stringify(result, null, "\t"));
     });

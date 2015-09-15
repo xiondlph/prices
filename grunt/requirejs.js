20f2f376e2cfa444f649cf19c9915b55335e578c
Ismailov Shukhrat <s.m.ismailov@tinkoff.ru>
2015-09-15 14:01:06 +0300

    Добавление ScrollSpy

--- grunt/requirejs.js
+++ grunt/requirejs.js
@@ -7,7 +7,7 @@ module.exports = {
                 text            : '../lib/requirejs/text',
                 jquery          : '../lib/jquery/jquery-2.1.1.min',
                 validator       : '../lib/validator.min',
-                scrollTo        : '../lib/scrollTo.min',
+                scrollspy       : '../lib/scrollspy',
                 underscore      : '../lib/underscore/underscore-min',
                 backbone        : '../lib/backbone/backbone-min',
                 localStorage    : '../lib/backbone/localStorage-min',
--- hosts/base/static/js/Index.js
+++ hosts/base/static/js/Index.js
@@ -13,7 +13,7 @@ require.config({
         text        : '../lib/requirejs/text',
         jquery      : '../lib/jquery/jquery-2.1.1.min',
         validator   : '../lib/validator.min',
-        scrollTo    : '../lib/scrollTo.min',
+        scrollspy   : '../lib/scrollspy',
         underscore  : '../lib/underscore/underscore-min',
         backbone    : '../lib/backbone/backbone-min',
 
@@ -30,32 +30,14 @@ require.config({
 
 require([
     'jquery',
-    'scrollTo',
+    'scrollspy',
     'View/Menu',
     'View/Signup',
     'View/Loader'
-], function ($, ScrollTo,  MenuView, SignupView, LoaderView) {
-
-    // Маршруты
-    function about() {
-        $('body').scrollTo($('section[name="about"]'), 800);
-    }
-
-    function destiny() {
-        $('body').scrollTo($('section[name="destiny"]'), 800);
-    }
-
-    function description() {
-        $('body').scrollTo($('section[name="description"]'), 800);
-    }
-
-    function terms() {
-        $('body').scrollTo($('section[name="terms"]'), 800);
-    }
+], function ($, Scrollspy,  MenuView, SignupView, LoaderView) {
 
     $(function () {
-        var router  = new Backbone.Router(),
-            signup  = new SignupView({obj: $('.b-section:first')}),
+        var signup  = new SignupView({obj: $('.b-section:first')}),
             menu    = new MenuView({el: $('.b-menu')}),
             loader  = new LoaderView({obj: $('body')});
 
@@ -63,11 +45,28 @@ require([
         signup.render();
         loader.render();
 
-        // Маршрутизация
-        router.route('about', 'about', about);
-        router.route('destiny', 'destiny', destiny);
-        router.route('description', 'description', description);
-        router.route('terms', 'terms', terms);
+        $('h1').on('scrollSpy:enter', function () {
+            console.log('enter:', $(this).attr('id'));
+            $('.b-nav__flow').removeClass('b-nav__flow_active');
+            $('a[href="#' + $(this).attr('id') + '"]').addClass('b-nav__flow_active');
+        });
+
+        $('h1').on('scrollSpy:exit', function () {
+            console.log('exit:', $(this).attr('id'));
+        });
+
+        $('.b-nav__flow').click(function (e) {
+            var href = $(this).attr("href"),
+                offsetTop = href === "#" ? 0 : $(href).offset().top - 110 + 1;
+
+            $('html, body').stop().animate({
+                scrollTop: offsetTop
+            }, 1000);
+
+            e.preventDefault();
+        });
+
+        $('h1').scrollSpy({offsetTop: 110});
 
         Backbone.history.start();
     });
